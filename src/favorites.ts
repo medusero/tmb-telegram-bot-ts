5a904636fb9b8b7b2f80245d003ca1eaf3e0dc62
medusero <234408661+medusero@users.noreply.github.com>
2026-09-05 18:37:31 +0200

    Añado funciones saveFavorites, deleteFavorites y listFavorites en favorites.ts y lo doy por cerrado

--- src/favorites.ts
+++ src/favorites.ts
@@ -1,7 +1,7 @@
 import { dirname, join, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { z, ZodError } from "zod";
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 import { AppError } from "./errors.js";
 
 const modulePath = fileURLToPath(import.meta.url);
@@ -32,6 +32,26 @@ class InvalidFavoritesShapeError extends AppError {
   }
 }
 
+class FavoritesSaveError extends AppError {
+  constructor(
+    cause: unknown,
+    message: string = "No se pudo guardar el archivo de favoritos",
+  ) {
+    super(message, { cause });
+    this.name = "FavoritesSaveError";
+  }
+}
+
+class FavoriteNotFoundError extends AppError {
+  constructor(
+    cause?: unknown,
+    message: string = "Ese alias no existe en el archivo",
+  ) {
+    super(message, { cause });
+    this.name = "FavoriteNotFoundError";
+  }
+}
+
 export async function loadFavorites(): Promise<Favorites> {
   try {
     const favoritesText = await readFile(cleanPath, "utf-8");
@@ -48,4 +68,28 @@ export async function loadFavorites(): Promise<Favorites> {
       throw error;
     }
   }
+}
+
+export async function saveFavorites(favorites: Favorites): Promise<void> {
+  try {
+    const favoritesFile = JSON.stringify(favorites, null, 4);
+    await writeFile(cleanPath, favoritesFile);
+  } catch (cause) {
+    throw new FavoritesSaveError(cause);
+  }
+}
+
+export async function deleteFavorites(alias: string): Promise<string> {
+  const favorites = await loadFavorites();
+  const { [alias]: extractedValue, ...rest } = favorites;
+  if (extractedValue === undefined) {
+    throw new FavoriteNotFoundError();
+  }
+  await saveFavorites(rest);
+  return extractedValue;
+}
+
+export async function listFavorites(): Promise<[string, string][]> {
+  const favorites = Object.entries(await loadFavorites());
+  return favorites;
 }
