3ea7108f75314fe69240686a150230e620a1bb76
medusero <234408661+medusero@users.noreply.github.com>
2026-09-09 09:01:43 +0200

    Add function handleSaveStep to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { getArrivals } from "./tmbApi.js";
-import { listFavorites } from "./favorites.js";
-import { Bot, Context, type NextFunction } from "grammy";
+import { addFavorite, listFavorites } from "./favorites.js";
 import { config } from "./config.js";
 import { formatArrivals, formatFavorites } from "./formatting.js";
 import { AppError } from "./errors.js";
+import {
+  Bot,
+  Context,
+  session,
+  type NextFunction,
+  type SessionFlavor,
+} from "grammy";
 
-const bot = new Bot(config.TELEGRAM_BOT_TOKEN);
+type NoneState = { state: "none" };
+type WaitingAliasState = { state: "waitingAlias" };
+type WaitingCodeState = { state: "waitingCode"; alias: string };
+type State = NoneState | WaitingAliasState | WaitingCodeState;
+type MyContext = Context & SessionFlavor<State>;
+
+const bot = new Bot<MyContext>(config.TELEGRAM_BOT_TOKEN);
 
 async function authResponse(ctx: Context, next: NextFunction): Promise<void> {
   if (ctx.from !== undefined && ctx.from.id === config.TELEGRAM_USER_ID) {
@@ -17,7 +29,51 @@ async function authResponse(ctx: Context, next: NextFunction): Promise<void> {
   }
 }
 
+function initial(): State {
+  return { state: "none" };
+}
+
+async function handleSaveStep(
+  ctx: MyContext,
+  next: NextFunction,
+): Promise<void> {
+  switch (ctx.session.state) {
+    case "none":
+      await next();
+      break;
+    case "waitingAlias":
+      if (!ctx.message?.text) {
+        await ctx.reply("Escríbeme el alias como mensaje de texto");
+      } else {
+        const alias = ctx.message?.text;
+        ctx.session = { state: "waitingCode", alias };
+        await ctx.reply("¿Cuál es el código de parada?");
+      }
+      break;
+    case "waitingCode":
+      if (!ctx.message?.text) {
+        await ctx.reply("Escríbeme el código como mensaje de texto");
+      } else {
+        const code = ctx.message?.text;
+        await getArrivals(code);
+        const alias = ctx.session.alias;
+        await addFavorite(alias, code);
+        await ctx.reply(`Nuevo favorito guardado: ${alias}: ${code}`);
+        ctx.session = { state: "none" };
+      }
+      break;
+    default: {
+      const exhaustiveCheck: never = ctx.session;
+      throw new Error(`Error inesperado: ${JSON.stringify(exhaustiveCheck)}`);
+    }
+  }
+}
+
 bot.use(authResponse);
+bot.use(session({ initial }));
+bot.use(handleSaveStep);
+bot.command("cancelar");
+bot.command("guardar");
 
 bot.hears(/^\d{1,4}$/, async (ctx) => {
   const stopCode = ctx.match[0];
